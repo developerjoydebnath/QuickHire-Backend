30f6c07cf37e08239c2143305531dfdfd5550bfd
developerjoydebnath <developerjoydebnath@gmail.com>
2026-03-02 00:57:20 +0600

    fix: bug fix

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -19,6 +19,8 @@ export const protect = async (req: Request, res: Response, next: NextFunction) =
 
   token = req.cookies.jwt;
 
+  console.log({token})
+
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET || 'fallback_secret') as JwtPayload;
